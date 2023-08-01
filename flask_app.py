321e39d3c42c02e952111c01ca54ab8f76f0ff7b
Felipe Sodré M. Barros <felipe.b4rros@gmail.com>
2023-08-01 17:32:02 -0300

    update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -16,6 +16,8 @@ from dotenv import load_dotenv
 from flask import Flask, request
 
 from dbhelper import DBHelper  # import class and method created to work with sqlite3
+from economybot import send_message, build_keyboard
+from messages import WELCOME_MSG
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -46,175 +48,140 @@ app = Flask(__name__)
 @app.route("/{}".format(PYANYWHERE_SECRET), methods=["POST"])
 def telegram_webhook():
     update = request.get_json()
-    if "message" in update:
-        user = update["message"]["from"]["first_name"]
-        text = update["message"]["text"]
-        chat_id = update["message"]["chat"]["id"]
-
-        if text == "/start":
-            bot.sendMessage(
-                chat_id,
-                "Welcome to Dosmestic Economy Bot! Your personal assistent, {}!!".format(
-                    user
-                ),
-                parse_mode="markdown",
-            )
-            users = db.get_users()
-            bot.sendMessage(chat_id, "{}!!".format(users), parse_mode="markdown")
-            if user not in users:
-                db.insertuser(user, chat_id)
-
-        if text.startswith("/expenses"):
-            if len(text.split(" ")) < 4:
-                bot.sendMessage(
-                    chat_id,
-                    "Sorry, I couldnt save your expenses. Something is missing",
-                    parse_mode="markdown",
-                )
+    categories = db.get_categories()
+    categories.append("CANCEL")
+    actions = db.get_actions()
+    actions.append("CANCEL")
+    try:
+        if "message" in update:
+            if "from" in update.get("message").keys():
+                user = update["message"]["from"]["first_name"]
             else:
-                action, value, category, subcategory = text.split(" ")
-                db.insertExpenses(
-                    user, category, subcategory, float(value), date.date.today()
-                )
-                bot.sendMessage(
-                    chat_id,
-                    "Ok, I'm done!\n {} inserted as expenses".format(value),
-                    parse_mode="markdown",
-                )
+                user = update["message"]["chat"]["first_name"]
+            registered_user = db.filter_user(user)
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
 
-        if text.startswith("/income"):
-            action, value = text.split(" ")
-            bot.sendMessage(chat_id, "Saving income!!", parse_mode="markdown")
-            db.insertIncome(user, value, date.date.today())
-            bot.sendMessage(
-                chat_id,
-                "Well done!\n {} inserted as income!".format(value),
-                parse_mode="markdown",
-            )
-
-        if text == "/category":
-            cats = db.get_category()
-            bot.sendMessage(
-                chat_id,
-                "Your options for **category** are:\n\n{}".format("\n".join(cats)),
-                parse_mode="markdown",
-            )
-
-        if text.startswith("/subcategory"):
-            if len(text.split(" ")) == 1:
-                subcats = db.get_subcategory()
-                bot.sendMessage(
-                    chat_id,
-                    "*Subcategory* options:\n\n{}".format("\n".join(subcats)),
-                    parse_mode="markdown",
+            if text == "/start":
+                send_message(
+                    f"Welcome to Dosmestic Economy Bot! Your personal assistent, {user}!!",
+                    chat,
                 )
-            if len(text.split(" ")) == 2:
-                command, cat = text.split(" ")
-                subcats = db.get_subcategory(cat)
-                bot.sendMessage(
-                    chat_id,
-                    "*Subcategory* options for the category *{}*, are:\n\n{}".format(
-                        cat, "\n".join(subcats)
-                    ),
-                    parse_mode="markdown",
-                )
+                if not registered_user:
+                    db.insertuser(user, chat)
+                db.clean_model()
+                keyboard = build_keyboard(actions)
+                send_message(WELCOME_MSG, chat, reply_markup=keyboard)
 
-        if text.startswith("/summary"):
-            if len(text.split(" ")) >= 2:
-                param = text.split(" ")[1]
-                if len(text.split(" ")) >= 3:
-                    month = text.split(" ")[2]
-                    month = month.zfill(2)
-                    year = date.date.today().year
-                    if len(text.split(" ")) == 4:
-                        year = text.split(" ")[3]
-                else:
-                    month = str(date.date.today().month).zfill(2)
-                    year = date.date.today().year
-                summary = db.get_summary(param, month, year)
-                bot.sendMessage(
-                    chat_id,
-                    "*Summary by {} for moth {} and year {}*:".format(
-                        param, month, year
-                    ),
-                    parse_mode="markdown",
-                )
-                bot.sendMessage(chat_id, "{}".format(summary), parse_mode="markdown")
-            else:
-                bot.sendMessage(
-                    chat_id,
-                    "*Wrong parameter sent!*\n you ust send:\n /summary [param] [month] [year]\n where [month] and [year] are optional",
-                    parse_mode="markdown",
-                )
+            elif text == "CANCEL":
+                db.clean_model()
+                keyboard = build_keyboard(actions)
+                send_message(WELCOME_MSG, chat, reply_markup=keyboard)
 
-        if text.startswith("/plot"):
-            if len(text.split(" ")) >= 2:
-                param = text.split(" ")[1]
-                if len(text.split(" ")) >= 3:
-                    month = text.split(" ")[2].zfill(2)
-                    year = date.date.today().year
-                    if len(text.split(" ")) == 4:
-                        year = text.split(" ")[3]
-                else:
-                    month = str(date.date.today().month).zfill(2)
-                    year = date.date.today().year
-                path = db.get_plots(param, month, year)
-                if isinstance(path, list):
-                    for plot in path:
-                        graph = open(relpath(plot), "rb")
-                        bot.sendPhoto(chat_id, graph)
-                elif path.startswith("Not"):
-                    bot.sendMenssage(chat_id, path)
-                else:
-                    graph = open(relpath(path), "rb")
-                    bot.sendPhoto(chat_id, graph)
-            else:
-                bot.sendMessage(
-                    chat_id,
-                    "*Wrong parameter sent!*\n you ust send:\n /plot [param] [month] [year]",
-                    parse_mode="markdown",
-                )
+            elif text == "expenses" and not db.status:
+                user = db.filter_user(user)
+                db.update_model_user(user.id)
+                action = db.filter_action(text)
+                db.update_model_action(action.id)
+                keyboard = build_keyboard(categories)
+                send_message("In which category?", chat, reply_markup=keyboard)
+                db.update_status("CATEGORY")
 
-        if text.startswith("/backup"):
-            bot.sendMessage(chat_id, "Building databse backup", parse_mode="markdown")
-            db.sqlite3_backup()
-            if len(text.split(" ")) == 2:
-                NO_OF_DAYS = int(text.split(" ")[1])
-                bot.sendMessage(
-                    chat_id,
-                    "Removing backups with {} days or more".format(NO_OF_DAYS),
-                    parse_mode="markdown",
-                )
-                db.clean_data(backup_dir="./backup", NO_OF_DAYS=NO_OF_DAYS)
-            bot.sendMessage(chat_id, "All done!", parse_mode="markdown")
+            elif db.status == "CATEGORY" and db.model.action_id:
+                category = db.filter_category(text)
+                db.update_model_category(category.id)
+                subcategories = db.get_subcategories()
+                subcategories.append("CANCEL")
+                keyboard = build_keyboard(subcategories)
+                send_message("In which subcategory?", chat, reply_markup=keyboard)
+                db.update_status("SUBCATEGORY")
 
-        if text.startswith("/sql"):
-            sql = text[5:]
-            msg = db.sql(sql)
-            bot.sendMessage(chat_id, "{}".format(msg), parse_mode="markdown")
+            elif db.status == "SUBCATEGORY" and db.model.category_id:
+                subcategory = db.filter_subcategory(text)
+                db.update_model_subcategory(subcategory.id)
+                keyboard = build_keyboard(["CANCEL"])
+                send_message("How much?", chat, reply_markup=keyboard)
+                db.update_status("VALUE")
 
-        if text.startswith("/add"):
-            if len(text.split(" ")) == 2:
-                cats = db.get_category()
-                value = text.split(" ")[1]
-                if value not in cats:
-                    sql = "INSERT INTO category(category) VALUES ('{}')".format(value)
-                    msg = db.sql(sql)
-                    bot.sendMessage(
-                        chat_id,
-                        "Value *{}* added on databse".format(value),
-                        parse_mode="markdown",
-                    )
-                else:
-                    msg = "Not processed: Category *{}* already exists;".format(value)
-                    bot.sendMessage(chat_id, msg, parse_mode="markdown")
-            if len(text.split(" ")) == 3:
-                value, svalue = text.split(" ")[1:]
-                sql = "INSERT INTO subcategory(catid, subcategory, category) VALUES ((select id from category where category = '{}'), '{}', '{}');".format(
-                    value, svalue, value
-                )
-                msg = db.sql(sql)
-                msg = "Value *{}* added on database!".format(svalue)
-                bot.sendMessage(chat_id, msg, parse_mode="markdown")
+            elif db.status == "VALUE" and db.model.subcategory_id:
+                val = float(text)
+                db.model.value = val
+                db.save_expenses()
+                keyboard = build_keyboard(actions)
+                send_message(WELCOME_MSG, chat, reply_markup=keyboard)
+                db.clean_model()
+    except Exception as e:
+        send_message(f"ERROR: {e}", chat)
+        #
+        #
+        # if text.startswith("/plot"):
+        #     if len(text.split(" ")) >= 2:
+        #         param = text.split(" ")[1]
+        #         if len(text.split(" ")) >= 3:
+        #             month = text.split(" ")[2].zfill(2)
+        #             year = date.date.today().year
+        #             if len(text.split(" ")) == 4:
+        #                 year = text.split(" ")[3]
+        #         else:
+        #             month = str(date.date.today().month).zfill(2)
+        #             year = date.date.today().year
+        #         path = db.get_plots(param, month, year)
+        #         if isinstance(path, list):
+        #             for plot in path:
+        #                 graph = open(relpath(plot), "rb")
+        #                 bot.sendPhoto(chat_id, graph)
+        #         elif path.startswith("Not"):
+        #             bot.sendMenssage(chat_id, path)
+        #         else:
+        #             graph = open(relpath(path), "rb")
+        #             bot.sendPhoto(chat_id, graph)
+        #     else:
+        #         bot.sendMessage(
+        #             chat_id,
+        #             "*Wrong parameter sent!*\n you ust send:\n /plot [param] [month] [year]",
+        #             parse_mode="markdown",
+        #         )
+        #
+        # if text.startswith("/backup"):
+        #     bot.sendMessage(chat_id, "Building databse backup", parse_mode="markdown")
+        #     db.sqlite3_backup()
+        #     if len(text.split(" ")) == 2:
+        #         NO_OF_DAYS = int(text.split(" ")[1])
+        #         bot.sendMessage(
+        #             chat_id,
+        #             "Removing backups with {} days or more".format(NO_OF_DAYS),
+        #             parse_mode="markdown",
+        #         )
+        #         db.clean_data(backup_dir="./backup", NO_OF_DAYS=NO_OF_DAYS)
+        #     bot.sendMessage(chat_id, "All done!", parse_mode="markdown")
+        #
+        # if text.startswith("/sql"):
+        #     sql = text[5:]
+        #     msg = db.sql(sql)
+        #     bot.sendMessage(chat_id, "{}".format(msg), parse_mode="markdown")
+        #
+        # if text.startswith("/add"):
+        #     if len(text.split(" ")) == 2:
+        #         cats = db.get_category()
+        #         value = text.split(" ")[1]
+        #         if value not in cats:
+        #             sql = "INSERT INTO category(category) VALUES ('{}')".format(value)
+        #             msg = db.sql(sql)
+        #             bot.sendMessage(
+        #                 chat_id,
+        #                 "Value *{}* added on databse".format(value),
+        #                 parse_mode="markdown",
+        #             )
+        #         else:
+        #             msg = "Not processed: Category *{}* already exists;".format(value)
+        #             bot.sendMessage(chat_id, msg, parse_mode="markdown")
+        #     if len(text.split(" ")) == 3:
+        #         value, svalue = text.split(" ")[1:]
+        #         sql = "INSERT INTO subcategory(catid, subcategory, category) VALUES ((select id from category where category = '{}'), '{}', '{}');".format(
+        #             value, svalue, value
+        #         )
+        #         msg = db.sql(sql)
+        #         msg = "Value *{}* added on database!".format(svalue)
+        #         bot.sendMessage(chat_id, msg, parse_mode="markdown")
 
     return "OK"
